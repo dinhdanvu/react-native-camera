1b0b10ae5a360c50080795035813af5038a20232
Nguyen Vinh Dinh <dinhdanvu@gmail.com>
2019-11-04 12:54:44 +0700

    delete (first in first out) if Storage SDCard is full

--- android/src/main/java/org/reactnative/camera/utils/RNFileUtils.java
+++ android/src/main/java/org/reactnative/camera/utils/RNFileUtils.java
@@ -1,19 +1,138 @@
 package org.reactnative.camera.utils;
 
-import android.content.Context;
 import android.net.Uri;
-
-
+import android.os.Environment;
+import android.os.StatFs;
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
 /**
- * Created by jgfidelis on 23/01/18.
+ * Edited by dinhdanvu on 23/01/18.
  */
 
 public class RNFileUtils {
+    private static void FindFirstDayVideoInPath(String directoryName, List<File> files) {
+        File directory = new File(directoryName);
 
+        // Get all files from a directory.
+        File[] fList = directory.listFiles();
+        if(fList != null)
+            for (File file : fList) {
+                if (file.isFile()) {
+                    files.add(file);
+                } else if (file.isDirectory()) {
+                    FindFirstDayVideoInPath(file.getAbsolutePath(), files);
+                }
+            }
+    }
+
+    private static String FindFirstDayVideo() {
+        File ext = Environment.getExternalStorageDirectory();
+        List<File> files = new ArrayList<>();
+        FindFirstDayVideoInPath(ext + "/CameraApp", files);
+        File[] fileArray =files.toArray(new File[files.size()]);
+        Arrays.sort(fileArray, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+
+        String filePath = FilenameUtils.getPath(fileArray[0].getPath());
+        return filePath;
+    }
+    private static void deleteRecursive(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory())
+            for (File child : fileOrDirectory.listFiles())
+                deleteRecursive(child);
+
+        fileOrDirectory.delete();
+    }
+    public static void DeleteFirst() {
+
+        String deletePath = FindFirstDayVideo();
+        File directory = new File(deletePath);
+        deleteRecursive(directory);
+    }
+
+    public static boolean externalMemoryAvailable() {
+        return android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED);
+    }
+
+    public static long getAvailableInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSizeLong();
+        long availableBlocks = stat.getAvailableBlocksLong();
+        return availableBlocks * blockSize;
+    }
+    public static String getAvailableInternalMemory() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSizeLong();
+        long availableBlocks = stat.getAvailableBlocksLong();
+        return formatSize(availableBlocks * blockSize);
+    }
+
+    public static long getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSizeLong();
+        long totalBlocks = stat.getBlockCountLong();
+        return totalBlocks * blockSize;
+    }
+
+    public static long getAvailableExternalMemorySize() {
+        if (externalMemoryAvailable()) {
+            File path = Environment.getExternalStorageDirectory();
+            StatFs stat = new StatFs(path.getPath());
+            long blockSize = stat.getBlockSizeLong();
+            long availableBlocks = stat.getAvailableBlocksLong();
+            return availableBlocks * blockSize;
+        } else {
+            return -1;
+        }
+    }
+
+    public static long getTotalExternalMemorySize() {
+        if (externalMemoryAvailable()) {
+            File path = Environment.getExternalStorageDirectory();
+            StatFs stat = new StatFs(path.getPath());
+            long blockSize = stat.getBlockSizeLong();
+            long totalBlocks = stat.getBlockCountLong();
+            return totalBlocks * blockSize;
+        } else {
+            return -1;
+        }
+    }
+
+    public static String formatSize(long size) {
+        String suffix = null;
+
+        if (size >= 1024) {
+            suffix = "KB";
+            size /= 1024;
+            if (size >= 1024) {
+                suffix = "MB";
+                size /= 1024;
+            }
+        }
+
+        StringBuilder resultBuffer = new StringBuilder(Long.toString(size));
+
+        int commaOffset = resultBuffer.length() - 3;
+        while (commaOffset > 0) {
+            resultBuffer.insert(commaOffset, ',');
+            commaOffset -= 3;
+        }
+
+        if (suffix != null) resultBuffer.append(suffix);
+        return resultBuffer.toString();
+    }
     public static File ensureDirExists(File dir) throws IOException {
         if (!(dir.isDirectory() || dir.mkdirs())) {
             throw new IOException("Couldn't create directory '" + dir + "'");
@@ -23,8 +142,11 @@ public class RNFileUtils {
 
     public static String getOutputFilePath(File directory, String extension) throws IOException {
         ensureDirExists(directory);
-        String filename = UUID.randomUUID().toString();
-        return directory + File.separator + filename + extension;
+        SimpleDateFormat formatter = new SimpleDateFormat("HH_mm_ss", Locale.US);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+
+        return directory + File.separator + fileName + extension;
     }
 
     public static Uri uriFromFile(File file) {
